4a0def3cdfc9a3eb38340fb13b28ca19b1d53391
Signe Kjær Brandt Hau <signehau@gmail.com>
2026-06-10 13:05:00 +0200

    Tekst på knappen rettet

--- components/ActivityCard.tsx
+++ components/ActivityCard.tsx
@@ -46,7 +46,7 @@ export function ActivityCard({ title, image }: ActivityProps) {
           onClick={() => router.push('blank/registration')}
           className="mx-auto mt-auto cursor-pointer"
         >
-          {language === "ua" ? "Отримати контакт" : "Bliv Kontakt"}
+          {language === "ua" ? "Отримати контакт" : "Bliv kontaktet"}
         </Button>
       </CardContent>
     </Card>
